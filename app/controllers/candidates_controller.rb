09ad8e8c7324454d97544bd76413f84256512f9a
Abhishek Kumar <abhishekkumar2718@gmail.com>
2019-06-08 12:54:01 +0530

    - Added links to elections#index, candidates#index and ledger in navbar - Improved index, show for election and candidates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,7 +1,14 @@
 class CandidatesController < ApplicationController
+  before_action :set_candidate, only: [:show]
   def index
   end
 
   def show
+  end
+
+  private
+
+  def set_candidate
+    @candidate = Candidate.find(params[:id])
   end
 end
